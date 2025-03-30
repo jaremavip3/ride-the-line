52472823e949227fa02cdc7d9a3e97ed1b7f5729
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-03-30 19:12:14 -0400

    Update Hero component styles for improved responsiveness and button design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ export default function Hero() {
   const customCSS = `
     @media (min-width: 624px) {
       .responsive-bg {
-       background-size: auto 100vh;
+       background-size: 100vw 110vh;
         background-image: ${desktopBackgroundImage};
         
       }
@@ -65,11 +65,11 @@ export default function Hero() {
         </div>
         <div className="flex-grow"></div>
 
-        <div className="flex flex-col justify-center items-center text-center gap-22 ">
+        <div className="flex flex-col justify-center items-center text-center gap-22 lg:flex-row lg:justify-between lg:mb-8">
           <h1 className="text-5xl lg:text-6xl font-extrabold text-white ">
             Ride the <span className="text-red-500/85 underline">Line</span>
           </h1>
-          <button className=" shadow-[0_0_0_3px_#000000_inset] px-9 py-3 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+          <button className="lg:px-12 lg:py-4 lg:border-white lg:text-white lg:shadow-[0_0_0_2px_#ffffff_inset] underline underline-offset-3 shadow-[0_0_0_3px_#000000_inset] px-9 py-3 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
             WATCH THE TRAILER
           </button>
         </div>
