2250029d07f694b79f72528e0f34dc8e00d89ac0
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-03-30 23:34:58 -0400

    Update Hero component styles for improved layout and background positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,8 +31,8 @@ export default function Hero() {
   const style = {
     height: "100vh",
     width: "100%",
-    minWidth: "320px",
-    backgroundPosition: " bottom",
+
+    backgroundPosition: "top",
 
     backgroundRepeat: "no-repeat",
   };
@@ -50,6 +50,7 @@ export default function Hero() {
      
         background-image: ${mobileBackgroundImage};
          background-size: auto 90vh;
+            
       }
        
     }
@@ -57,21 +58,24 @@ export default function Hero() {
 
   return (
     <>
+      {" "}
       <style>{customCSS}</style>
-      <section className="responsive-bg px-10 pt-8 pb-6 text-white flex flex-col justify-between" style={style}>
-        <div className="flex flex-row justify-between items-center font-bold text-xl gap-30 text-center lg:text-5xl lg:font-extrabold  ">
-          <span className="text-white  ">ABOUT THE FILM</span>
-          <span className="text-white  ">HOST A SCREENING</span>
-        </div>
-        <div className="flex-grow"></div>
+      <section className="bg-red-600/85">
+        <div className="responsive-bg px-10 pt-8 pb-6 text-white flex flex-col justify-between" style={style}>
+          <div className="flex flex-row justify-between items-center font-bold text-xl gap-30 text-center lg:text-5xl lg:font-extrabold  ">
+            <span className="text-white  ">ABOUT THE FILM</span>
+            <span className="text-white  ">HOST A SCREENING</span>
+          </div>
+          <div className="flex-grow"></div>
 
-        <div className="flex flex-col justify-center items-center text-center gap-22 lg:flex-row lg:justify-between lg:mb-8">
-          <h1 className="text-5xl lg:text-6xl font-extrabold text-white ">
-            Ride the <span className="text-red-500/85 underline">Line</span>
-          </h1>
-          <button className="lg:px-12 lg:py-4 lg:border-white lg:text-white lg:shadow-[0_0_0_2px_#ffffff_inset] underline underline-offset-3 shadow-[0_0_0_3px_#000000_inset] px-9 py-3 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
-            WATCH THE TRAILER
-          </button>
+          <div className="flex flex-col justify-center items-center text-center gap-22 lg:flex-row lg:justify-between lg:mb-8">
+            <h1 className="text-5xl lg:text-6xl font-extrabold text-white ">
+              Ride the <span className="text-red-500/85 underline">Line</span>
+            </h1>
+            <button className="lg:px-12 lg:py-4 lg:border-white lg:text-white lg:shadow-[0_0_0_2px_#ffffff_inset] underline underline-offset-3 shadow-[0_0_0_3px_#000000_inset] px-9 py-3 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">
+              WATCH THE TRAILER
+            </button>
+          </div>
         </div>
       </section>
     </>
