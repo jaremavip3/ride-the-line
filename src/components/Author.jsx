80122139f85741f510b416eb94267ed6b729a8e2
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-04-07 22:44:48 -0400

    Update Author component for improved layout and  edited biography content

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -10,29 +10,28 @@ export default function Author() {
         </div>
 
         {/* Фото і біографія */}
-        <div className="flex flex-col md:flex-row gap-8 items-center mb-12">
+        <div className="flex flex-col md:flex-row gap-8  mb-12">
           {/* Фото режисера */}
-          <div className="w-48 h-48 md:w-56 md:h-56 overflow-hidden rounded-full border-3 border-red-500/85 flex-shrink-0">
+          <div className="w-48 h-48 mx-auto  overflow-hidden rounded-full border-3 border-red-500/85 flex-shrink-0">
             <Image src={author_image} alt="Ihor Volochii" />
           </div>
 
           {/* Біографія */}
           <div>
-            <h3 className="text-xl font-bold mb-4 md:text-left text-center text-gray-900">
-              Director Biography - <span className="text-red-600">Ihor Volochii</span>
-            </h3>
+            <h3 className="text-xl font-bold mb-4 md:text-left text-center text-red-600">Ihor Volochii</h3>
             <p className="text-pretty text-lg leading-relaxed tracking-wide text-gray-600 font-medium">
-              Ihor Volochii is a 34-year-old avid cyclist, sports and adventure filmmaker from Ukraine. With the
-              beginning of the full-scale invasion Ihor is actively involved in volunteering.
+              Ihor Volochii is a 34-year-old passionate cyclist and adventure filmmaker from Ukraine. With the beginning
+              of the full-scale invasion Ihor has dedicated himself to volunteer efforts, channeling his skills and
+              energy to support those in need.
             </p>
           </div>
         </div>
 
         {/* Заява режисера */}
         <div className="relative">
-          <h3 className="text-xl font-bold mb-4 md:text-center text-left  text-gray-900">Director Statement</h3>
+          <h3 className="text-xl font-bold mb-4  text-left  text-gray-900">Director Statement</h3>
           <p className="text-pretty text-lg leading-relaxed tracking-wide text-gray-600 md:text-left font-medium">
-            When I first thought about making videos as a profession I supposed that it will teach me to see all the
+            «When I first thought about making videos as a profession I supposed that it will teach me to see all the
             best in the world at different angles. But nowadays reality is that I pass through the dilapidated towns and
             villages of my country and listen to stories that bring goosebumps and tears to my eyes.
           </p>
@@ -43,12 +42,13 @@ export default function Author() {
           </p>
 
           <p className="text-pretty text-lg leading-relaxed tracking-wide text-gray-600 mt-6 md:text-left font-medium">
-            I'm really addicted to cycling, but even more I{" "}
-            <span className="text-red-600 font-bold">love my country</span>, and this film was born out of this love.
+            While I’m truly passionate about cycling, my{" "}
+            <span className="text-red-600 font-bold">love for my country</span>runs far deeper. And this film is about
+            that love — a tribute to my homeland, its history, and the unwavering spirit of its people»
           </p>
 
           {/* Декоративна лінія внизу */}
-          <div className="h-0.5 w-30 bg-red-500/50 mx-auto mt-10"></div>
+          <div className="h-0.5 w-52 bg-red-500/50 mx-auto mt-10"></div>
         </div>
       </div>
     </section>
