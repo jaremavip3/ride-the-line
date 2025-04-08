1a50e0da29db7f2657c0d1b9a27a699c15a1e9bb
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-04-07 22:44:01 -0400

    Changed decorative line width

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,7 +1,7 @@
 export default function Sponsors() {
   return (
     <section className="">
-      <div className="h-0.5 w-30 bg-red-500/50 mx-auto mt-10"></div>
+      <div className="h-0.5 w-52 bg-red-500/50 mx-auto mt-10"></div>
       Sponsors
     </section>
   );
